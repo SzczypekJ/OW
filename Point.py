e559bb02aff48b2ee29e0a263fa2706d855c25f6
SzczypekJ <szczypek@student.agh.edu.pl>
2024-10-28 12:20:11 +0100

    start2

--- Point.py
+++
@@ -1,3 +0,0 @@
-class Point:
-    def __init__(self, number_of_cryteria) -> None:
-        self.number_of_cryteria = number_of_cryteria
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import typing
+import numpy as np
 
 class ComparisonCounter:
     def __init__(self):
@@ -8,3 +10,9 @@ class ComparisonCounter:
     def le(self, x, y):
         self.comparison_count_coords += 1
         return x <= y
+    
+
+class Point:
+    def __init__(self, number_of_cryteria: int, point_coordinates: np.ndarray[float]) -> None:
+        self.number_of_cryteria = number_of_cryteria
+        self.vector = point_coordinates
